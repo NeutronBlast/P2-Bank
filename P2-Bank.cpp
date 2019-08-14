263e850f20de8cbd22e147f58217ec2f3f6e7793
Frank Hesse <45906155+metalsonic21@users.noreply.github.com>
2019-08-14 01:32:47 -0400

    insert in client list and accounts list
    accounts list insertion is bugged

--- P2-Bank.cpp
+++ P2-Bank.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {FALSE = 0, TRUE} boolean;
+
+
 typedef struct trans {
     int code;
     int issuersAccsID;
@@ -106,7 +109,157 @@ void linkAccountMenu(){
         }
 }
 
-void loadFromFile(){
+void fileExists(FILE *input, client **p){
+    char line[1000]; char dat[1000];
+    client *ax = new client;
+    client *t = *p;
+    int clients = 0, accounts = 0, data = 0, j=0;
+    boolean listClients = FALSE;
+    boolean listAccounts = FALSE;
+
+/************************* AUX VARS *********************/
+    int auxid = 0, auxt = 0;
+    char auxb [50];
+    double auxbal = 0;
+
+        while (!feof(input) && !ferror(input)){
+            fgets(line, 1000, input);
+            //printf("%s\n",line);
+            if (line[0]=='*' && line[1]=='C'){
+                ax = new client;
+                accounts=0;
+                listClients = TRUE;
+                listAccounts=FALSE;
+            }
+
+            if (listClients == TRUE && line[0]!='*' && line[1]!='C'){
+                for (int i=0; i<=strlen(line); i++){
+                    /* For each '-' it's a different variable to fill in the list */
+                    if (line[i]!='-'){
+                        dat[j]=line[i];
+                        j++;
+                    }
+                    if (line[i]=='-' || i==strlen(line)){
+                        dat[j]='\0';
+                        j=0;
+                        //printf("dat %s\n", dat);
+                        switch (data)
+                        {
+                        case 0:
+                            ax->id = strtol(dat, NULL, 10);
+                            //printf("Cedula %d\n", ax->id);
+                            break;
+                        case 1: 
+                            strcpy(ax->name,dat);
+                            break;
+                        case 2:
+                            strcpy(ax->city,dat);
+                            break;
+                        case 3: 
+                            strcpy(ax->phone,dat);
+                            data=-1;
+                            break;
+                        }
+                        strcpy(dat,"");
+                        data++;
+                        continue;
+                    } 
+                } //End of for
+
+                if (clients==0){
+                /*Insert at the beginning of the list*/
+                    ax->next = *p;
+                    *p = ax;
+                }
+                else{
+                /*Insert at the end of the list*/
+                    t = *p;
+                    ax->next = NULL;
+                        while(t->next != NULL)
+                            t=t->next;
+                    t->next = ax;
+                }
+                clients++;
+                listClients=FALSE;
+            } //End of if
+
+/******************************************* ACCOUNTS *************************************************/
+
+            if (line[0]=='*' && line[1]=='B'){
+                listAccounts = TRUE;
+            }
+
+            if (listAccounts == TRUE && line[0]!='*' && line[1]!='B'){
+                printf("here\n");
+                for (int i=0; i<=strlen(line); i++){
+                    /* For each '-' it's a different variable to fill in the list */
+                    if (line[i]!='-'){
+                        dat[j]=line[i];
+                        j++;
+                    }
+                    if (line[i]=='-' || i==strlen(line)){
+                        dat[j]='\0';
+                        j=0;
+                        //printf("dat %s\n", dat);
+                        switch (data)
+                        {
+                        case 0:
+                            auxid = strtol(dat, NULL, 10);
+                            printf("Numero de cuenta %d\n", auxid);
+                            break;
+                        case 1: 
+                            strcpy(auxb,"");
+                            strcpy(auxb,dat);
+                            break;
+                        case 2:
+                            auxt = strtol(dat,NULL,10);
+                            break;
+                        case 3: 
+                            char *ptr;
+                            auxbal=strtod(dat, &ptr);
+                            printf("Balance %lf\n", auxbal);
+                            data=-1;
+                            break;
+                        }
+                        strcpy(dat,"");
+                        data++;
+                        continue;
+                    } 
+                } //End of for
+
+                /*Insert at the end of the list*/
+                    ax->anext = new account;
+                    t = *p;
+                    t->anext = (*p)->anext;
+                    ax->anext->id = auxid;
+                    strcpy(ax->anext->bank,auxb);
+                    ax->anext->type = auxt;
+                    ax->anext->balance = auxbal;
+                    ax->anext->next = NULL;
+                        if (accounts == 0)
+                            (*p)->anext = ax->anext;
+                        else while(t->anext->next != NULL)
+                            t->anext=t->anext->next;
+                    t->anext->next = ax->anext;
+                accounts++;
+            } //End of if
+
+
+/***************************************** TRANSACTIONS *************************************************/
+
+            if (line[0]=='*' && line[1]=='T'){
+                accounts=0;
+                listAccounts=FALSE;
+                //ax->anext = new account;
+                //listAccounts = TRUE;
+            }
+
+            strcpy(line,"");    
+        } //End of while !(eof)
+    fclose(input);
+}
+
+void loadFromFile(client **p){
     char path[1024];
     FILE *fp;
         printf("Especifique ruta de archivo\n");
@@ -117,6 +270,8 @@ void loadFromFile(){
             }
             else {
                 printf("Archivo cargado con exito\n");
+                fileExists(fp, p);
+                system("pause");
             }
 }
 
@@ -124,14 +279,14 @@ void showMenu(){
     client *c = NULL;
     int op=-1;
         while (op){
-            printf("*** Sistema de pagos y cobros P2Bank ***\n\n");
+            printf("\n*** Sistema de pagos y cobros P2Bank ***\n\n");
             printf("    \t 1. Cuentas\n");
             printf("    \t 2. Opciones de pagos\n");
             printf("    \t 3. Consultas varias\n");
             printf("    \t 4. Cargar datos desde archivo\n");
             printf("    \t 5. Mostrar todos los datos\n");
             //Option 5 will be used to make the output file because the program has to access the whole nested list anyway
-            printf("    \t 6. Insertar datos manualmente");
+            printf("    \t 6. Insertar datos manualmente\n");
             //Option 6 will be used for something in option 1, for now it's just testing
             printf("    \t 0. Salir\n");
             scanf("%d",&op);
@@ -150,7 +305,8 @@ void showMenu(){
                 break;
 
             case 4:
-                loadFromFile();
+                loadFromFile(&c);
+                system("cls");
                 break;
 
             case 0:
